a279db830a2707c79e6decbbe1b54dbf9ff75460
Eatoes <development@eatoes.com>
2025-11-27 07:33:28 +0530

    oxymorons

---
+++ config.js
@@ -0,0 +1,14 @@
+window.SITE_CONFIG = {
+  title: 'Oxymorons',
+  tradeName: 'Oxymorons',
+  legalName: 'OXYMORONS HOSPITALITY LLP',
+  phoneNumber: '8019987878',
+  gstNumber: '36ABEFB2212Q1ZF',
+  address: 'FLAT NO 103, ROAD NO 12, BANJARA HILLS,  Secunderabad, Hyderabad, Telangana, 500034',
+  openingHours: {
+    monFri: 'MON - FRI: 12PM - 10PM',
+    saturday: 'SATURDAY: 12PM - 12AM',
+    sunday: 'SUNDAY: 12PM - 11PM'
+  },
+  facebookDomainVerification: 'htc93c242cspng4nspahnad95t4nux'
+};
